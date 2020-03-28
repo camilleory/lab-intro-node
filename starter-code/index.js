a71ae2e891dec0d62a486a03291c430c877b7e4d
Camille Ory <camilla.ory@gmail.com>
2020-03-28 16:56:31 +0100

    lab almost finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    return this.items;
+  }
 
-  get(pos) {}
 
-  max() {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let highestValue = Math.max(...this.items);
+      return highestValue;
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let lowestValue = Math.min(...this.items);
+      return lowestValue;
+    }
+  }
+
+
+  sum() {
+    if (this.items.length <= 0) {
+      return 0
+    } else {
+      this.items.reduce(
+        (accumulator, currentValue) =>
+        accumulator + currentValue, 0
+      )
+    }
+  }
+
 
   avg() {}
 }
 
 module.exports = SortedList;
+
+//let obj = new SortedList(items, length);
